bd4df6402040abdbae1cdea50107d0a634895621
JainStuti25 <41702974+JainStuti25@users.noreply.github.com>
2020-01-28 23:02:17 +0530

    Initial commit

---
+++ app/src/main/java/www/xyz/animatelogin/MainActivity.kt
@@ -0,0 +1,27 @@
+package www.xyz.animatelogin
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val animationDrawable = ianimate.background as AnimationDrawable
+//        animationDrawable.setEnterFadeDuration(2000)
+//        animationDrawable.setExitFadeDuration(3000)
+        animationDrawable.start()
+
+
+    }
+}
